a93abb2a1b17117693f1c4d5545ba86a83496876
ZyadHany35 <86461552+ZyadHany35@users.noreply.github.com>
2023-12-05 14:37:31 +0200

    Update BigReal.cpp

--- BigReal.cpp
+++ BigReal.cpp
@@ -4,12 +4,101 @@
 #include <string>
 using namespace std;
 
-bool BigReal:: isValidReal(const string& real){
-        if (regex_match(real, regex("[+-]?\\d*\\.?\\d*")))
-            return true;
+bool BigReal::isValidReal(const string& real)
+{
+    if (regex_match(real, regex("[+-]?\\d*\\.?\\d*"))) 
+        return true;
 
-        else  return false;
+    else  return false;
+
+};
+
+BigReal::BigReal(string real) {
+    if (isValidReal(real)) {
+        if (real[0] == '-') {
+            sign = '-';
+            real[0] = '0';
+        }
+        else if (real[0] == '+') {
+            sign = '+';
+            real[0] = '0';
+        }
+        if (regex_search(real, regex("\\."))) { // if has a dot 
+
+            integer = real.substr(0, real.find('.'));
+            fraction = real.substr(real.find('.') + 1);
+            if (integer.size() == 0) 
+                integer = "0";
+            if (fraction.size() == 0) 
+                fraction = "0";
+            
+        }
+        else {
+            if (real.size() == 0)
+                integer = "0";
+            else
+                integer = move(real);
+            fraction = "0";
+        }
+        integer.erase(0, integer.find_first_not_of('0'));
+        fraction.erase(fraction.find_last_not_of('0')+1,fraction.size()-1);
+
+
+        if (integer.empty())
+            integer = "0";
+        if (fraction.empty())
+            fraction = "0";
+    }
+    else {
+        integer = "0"; // if not valid store +0.0
+        fraction = "0";
+    }
 }
+
+void BigReal::setNum(string real) {
+    if (isValidReal(real)) {
+        if (real[0] == '-') {
+            sign = '-';
+            real[0] = '0';
+        }
+        else if (real[0] == '+') {
+            sign = '+';
+            real[0] = '0';
+        }
+        if (regex_search(real, regex("\\."))) { // if has a dot 
+
+            integer = real.substr(0, real.find('.'));
+            fraction = real.substr(real.find('.') + 1);
+            if (integer.size() == 0) 
+                integer = "0";
+            if (fraction.size() == 0) 
+                fraction = "0";
+            
+        }
+        else {
+            if (real.size() == 0)
+                integer = "0";
+            else
+                integer = move(real);
+            fraction = "0";
+        }
+        integer.erase(0, integer.find_first_not_of('0'));
+        fraction.erase(fraction.find_last_not_of('0')+1,fraction.size()-1);
+
+
+        if (integer.empty())
+            integer = "0";
+        if (fraction.empty())
+            fraction = "0";
+    }
+    else {
+        integer = "0"; // if not valid store +0.0
+        fraction = "0";
+    }
+}
+
+//BigReal::BigReal(const BigReal& other); // Copy constructor
+//BigReal::BigReal& operator= (BigReal& other); // Assignment operator
 
 BigReal BigReal::operator+ (const BigReal& other) {  
     string sumInt = "", sumFrac = "";
@@ -158,7 +247,107 @@ BigReal BigReal::operator- (const BigReal& other) {
     return BigReal(sign, subInt, subFrac);
 }
 
-ostream& operator << (ostream& out, const BigReal& num){
+bool BigReal::operator< (const BigReal& anotherReal) {
+    if (sign == '+' && anotherReal.sign == '-') {
+        return false;
+    }
+    else if (sign == '-' && anotherReal.sign == '+')
+        return true;
+    string integer1 = integer, fraction1 = fraction; // to not modify the original real 
+    string integer2 = anotherReal.integer, fraction2 = anotherReal.fraction;
+    int nonZeroIndex1=0;
+    int nonZeroIndex2=0;
+    if(integer != "0")
+        nonZeroIndex1 = integer1.find_first_not_of('0');
+    if(anotherReal.integer != "0")
+        nonZeroIndex2 = integer2.find_first_not_of('0');
+
+    // Compare the lengths after removing leading zeros
+    int len1 = integer1.length() - nonZeroIndex1;
+    int len2 = integer2.length() - nonZeroIndex2;
+    bool ifSign = sign == '+' ? true : false;
+    if (len1 < len2) {
+        return ifSign;
+    }
+    else if (len1 > len2)
+        return !ifSign;
+
+    for (int i = 0; i < len1; ++i) {
+        if (integer1[nonZeroIndex1 + i] > integer2[nonZeroIndex2 + i])
+            return !ifSign;
+        else if (integer1[nonZeroIndex1 + i] < integer2[nonZeroIndex2 + i])
+            return ifSign;
+    }
+    if (fraction1.length() > fraction2.length())
+        fraction2.resize(fraction1.length(), '0');
+    else if (fraction1.length() < fraction2.length())
+        fraction1.resize(fraction2.length(), '0');
+
+    for (int j = 0; j < fraction1.length(); ++j) {
+        if (fraction1[j] > fraction2[j])
+            return !ifSign;
+        else if (fraction1[j] < fraction2[j])
+            return ifSign;
+    }
+    return false;
+    
+}
+
+bool BigReal::operator> (const BigReal& anotherReal) {
+    if (sign == '+' && anotherReal.sign == '-') {
+        return true;
+    }
+    else if (sign == '-' && anotherReal.sign == '+')
+        return false;
+    string integer1 = integer, fraction1 = fraction; // to not modify the original real 
+    string integer2 = anotherReal.integer, fraction2 = anotherReal.fraction;
+
+    int nonZeroIndex1 = 0;
+    int nonZeroIndex2 = 0;
+    if (integer != "0")
+        nonZeroIndex1 = integer1.find_first_not_of('0');
+    if (anotherReal.integer != "0")
+        nonZeroIndex2 = integer2.find_first_not_of('0');
+
+    // Compare the lengths after removing leading zeros
+    int len1 = integer1.length() - nonZeroIndex1;
+    int len2 = integer2.length() - nonZeroIndex2;
+    bool ifSign = sign == '+' ? false : true;
+    if (len1 < len2) 
+        return ifSign;
+    else if (len1 > len2)
+        return !ifSign;
+
+    for (int i = 0; i < len1; ++i) {
+        if (integer1[nonZeroIndex1 + i] > integer2[nonZeroIndex2 + i])
+            return !ifSign;
+        else if (integer1[nonZeroIndex1 + i] < integer2[nonZeroIndex2 + i])
+            return ifSign;
+    }
+    if (fraction1.length() > fraction2.length())
+        fraction2.resize(fraction1.length(), '0');
+    else if (fraction1.length() < fraction2.length())
+        fraction1.resize(fraction2.length(), '0');
+
+    for (int j = 0; j < fraction1.length(); ++j) {
+        if (fraction1[j] > fraction2[j])
+            return !ifSign;
+        else if (fraction1[j] < fraction2[j])
+            return ifSign;
+
+    }
+    return false;
+    
+}
+bool BigReal::operator== (const BigReal& anotherReal) {
+    if (!(*this > anotherReal) && !(*this < anotherReal))
+        return true;
+    else
+        return false;
+}
+
+ostream& operator << (ostream& out, const BigReal& num)
+{
     if (num.sign == '+')
     {
         out << num.integer << '.' << num.fraction;
@@ -169,7 +358,4 @@ ostream& operator << (ostream& out, const BigReal& num){
     }
     return out;
 }
-
-
-
 
